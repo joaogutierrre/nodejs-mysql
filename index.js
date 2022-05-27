c065ba1d849ee015f70539ee04dedb14d15fa054
suporte <suporte@CITDF1PC3872.cit.local>
2022-05-27 18:21:15 -0300

    feat: Insert query to create new database

--- index.js
+++ index.js
@@ -16,3 +16,13 @@ db.connect((err) => {
   }
   console.log("Yay, Connection established to DB!!!");
 });
+
+//Create database
+app.get('/createdb', (req,res){
+    db.query(sql, (err)=>{
+        if(err){
+            throw err;
+        }
+        res.send("Database created!!!!");
+    });
+})
