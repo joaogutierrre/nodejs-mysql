39cc58f876c6fb14360e494ac7304fdf3da8b0a8
João Gutierre <gutierrejoao@gmail.com>
2022-05-27 18:13:21 -0300

    feat: Create and establish connection to DB

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const express = require('express')
-const mysql = require('mysql')
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+
+//Create db connection
+const db = mysql.createConnection({
+  host: localhost,
+  user: root,
+  password: 1234,
+});
+
+//Conenct to db
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Yay, Connection established to DB!!!");
+});
