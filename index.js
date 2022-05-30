d5f578a5e21d7cf1d63213c8070bc2569ffc03ed
João Gutierre <joao.gutierre@centralit.com.br>
2022-05-30 01:34:59 -0300

    feat: Writing query to create the Employee table on database.

--- index.js
+++ index.js
@@ -27,3 +27,15 @@ app.get("/createdb", (req, res) => {
     res.send("Database created!!!!");
   });
 });
+
+//Create table
+app.get("/createemployee", (req, res) => {
+  let sql =
+    "CREATE TABLE employee(id int AUTO_INCREMENT, name VARCHAR(255), designation VARCHAR(255), PRIMARY KEY(id)";
+  db.query(sql, (err) => {
+    if (err) {
+      throw err;
+    }
+    res.send("Emplyee table created");
+  });
+});
